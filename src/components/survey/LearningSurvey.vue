04b7e76f524dc8cb1dd50f1fb1628d1fa046b589
VA Jonathan Ong <100127728+Jon-vainsight@users.noreply.github.com>
2022-03-10 15:11:05 +0800

    Connected to backend

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -9,7 +9,13 @@
         </div>
         <h3>My learning experience was ...</h3>
         <div class="form-control">
-          <input type="radio" id="rating-poor" value="poor" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-poor"
+            value="poor"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-poor">Poor</label>
         </div>
         <div class="form-control">
@@ -23,12 +29,18 @@
           <label for="rating-average">Average</label>
         </div>
         <div class="form-control">
-          <input type="radio" id="rating-great" value="great" name="rating" v-model="chosenRating" />
+          <input
+            type="radio"
+            id="rating-great"
+            value="great"
+            name="rating"
+            v-model="chosenRating"
+          />
           <label for="rating-great">Great</label>
         </div>
-        <p
-          v-if="invalidInput"
-        >One or more input fields are invalid. Please check your provided data.</p>
+        <p v-if="invalidInput">
+          One or more input fields are invalid. Please check your provided data.
+        </p>
         <div>
           <base-button>Submit</base-button>
         </div>
@@ -59,6 +71,20 @@ export default {
         userName: this.enteredName,
         rating: this.chosenRating,
       });
+
+      fetch(
+        'https://vue-https-demo-2e251-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: this.enteredName,
+            rating: this.chosenRating,
+          }),
+        }
+      );
 
       this.enteredName = '';
       this.chosenRating = null;
